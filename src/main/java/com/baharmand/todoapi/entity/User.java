a00d2018ff561194221b5af497087311c713eb06
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-31 15:17:58 +0100

    Create Role and User entities

---
+++ src/main/java/com/baharmand/todoapi/entity/User.java
@@ -0,0 +1,46 @@
+package com.baharmand.todoapi.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @Column(updatable = false)
+    private String email;
+    @Column(nullable = false, length = 100)
+    private String password;
+    private boolean expired;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public void addRole(Role role) {
+        if (role == null) throw new IllegalArgumentException("Role is null.");
+        if (roles == null) roles = new HashSet<>();
+        roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        if (role == null) throw new IllegalArgumentException("Role is null.");
+        if (roles != null) {
+            roles.remove(role);
+        } else {
+            // todo: throw exception if needed ...
+        }
+    }
+}
