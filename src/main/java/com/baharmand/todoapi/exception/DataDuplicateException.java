8fd1c2628b754fae741e8a34854c74594b431006
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-01 08:08:42 +0100

    Create custom exceptions

---
+++ src/main/java/com/baharmand/todoapi/exception/DataDuplicateException.java
@@ -0,0 +1,12 @@
+package com.baharmand.todoapi.exception;
+
+public class DataDuplicateException extends RuntimeException {
+    public DataDuplicateException(String message) {
+        super(message);
+    }
+
+    public DataDuplicateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
