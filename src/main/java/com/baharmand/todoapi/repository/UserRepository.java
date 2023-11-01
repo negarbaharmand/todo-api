0c16b8f36aae87959a5f41d062e86ca8b2d7aa40
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-01 08:58:06 +0100

    Add findByEmail method to UserRepository

--- src/main/java/com/baharmand/todoapi/repository/UserRepository.java
+++ src/main/java/com/baharmand/todoapi/repository/UserRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     Boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
 
     @Modifying
     @Query("update User u set u.expired = :status where u.email = :email")
