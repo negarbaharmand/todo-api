80c413fb87f4b42434b61154aed74252ed6ef1df
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-09 17:01:56 +0100

    Replace @Data annotation for Lombok annotations (@Setter, @Getter, @AllArgsConstructor, @NoArgsConstructor)

--- src/main/java/com/baharmand/todoapi/domain/dto/UserDTOForm.java
+++ src/main/java/com/baharmand/todoapi/domain/dto/UserDTOForm.java
@@ -1,16 +1,10 @@
 package com.baharmand.todoapi.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Set;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class UserDTOForm {
     private String email;
     private String password;
