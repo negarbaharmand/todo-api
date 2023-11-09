819ca4fac489d09bdb3cdd753cefa33f0dabbd57
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-09 17:02:19 +0100

    Implement RoleController class

---
+++ src/main/java/com/baharmand/todoapi/controller/RoleController.java
@@ -0,0 +1,29 @@
+package com.baharmand.todoapi.controller;
+
+import com.baharmand.todoapi.domain.dto.RoleDTOView;
+import com.baharmand.todoapi.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/roles")
+@RestController
+public class RoleController {
+
+    RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleDTOView>> doGetRoles() {
+    List<RoleDTOView> responseBody = roleService.getAll();
+    return ResponseEntity.ok().body(responseBody);
+    }
+}
